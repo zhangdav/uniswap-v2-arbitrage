f113a027305dc9f50b29508245de3fc204ca0db9
DAVID ZHANG <d12stan@hotmail.com>
2025-10-07 13:53:41 +0700

    init

---
+++ src/UniswapV2Arbitrage.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {IUniswapV2Pair} from "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+import {IUniswapV2Router02} from "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract UniswapV2Arbitrage {
+
+}
